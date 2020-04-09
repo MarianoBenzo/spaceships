a78ea71dd0b58de59c4372cf164cf595cec42fa9
mariano.benzo <marianobenzo@gmail.com>
2020-04-09 06:32:26 -0300

    Resize canvas y nuevo fondo

--- src/client/services/CanvasService.ts
+++ src/client/services/CanvasService.ts
@@ -28,6 +28,15 @@ class CanvasService {
     canvas.height = this.height;
 
     this.ctx = canvas.getContext('2d');
+
+    window.addEventListener('resize', () => this.resizeCanvas(canvas))
+  }
+
+  resizeCanvas(canvas: HTMLCanvasElement) {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
   }
 
   drawUniverse(universe: Universe, id: string) {
@@ -50,22 +59,42 @@ class CanvasService {
   }
 
   drawBackground(universeWidth: number, universeHeight: number) {
-    for (let i = 0; i < universeHeight; i += 50) {
-      for (let j = 0; j < universeWidth; j += 50) {
-        if ((i / 50 + j / 50) & 1) {
-          this.ctx.fillStyle = "hsl(" + (360 - (i + j) / 10) + ", 50%, 70%)";
-          this.ctx.fillRect(j + this.viewport.x, i + this.viewport.y, 50, 50);
-          //this.ctx.fillRect(j, i, 50, 50);
-        }
-      }
+    const fillColor = 'rgba(255, 255, 255)';
+    const strokeColor = 'rgba(221, 221, 221)';
+
+    this.ctx.fillStyle = fillColor;
+    this.ctx.fillRect(this.viewport.x, this.viewport.y, universeWidth, universeHeight);
+
+    this.ctx.beginPath();
+    for (let x = this.viewport.x; x < universeWidth + this.viewport.x; x += 50) {
+      this.ctx.moveTo(x, this.viewport.y);
+      this.ctx.lineTo(x, universeHeight + this.viewport.y);
     }
+    this.ctx.moveTo(universeWidth + this.viewport.x, this.viewport.y);
+    this.ctx.lineTo(universeWidth + this.viewport.x, universeHeight + this.viewport.y);
+
+    for (let y = this.viewport.y; y < universeHeight + this.viewport.y; y += 50) {
+      this.ctx.moveTo(this.viewport.x, y);
+      this.ctx.lineTo(universeWidth + this.viewport.x, y);
+    }
+    this.ctx.moveTo(this.viewport.x, universeHeight + this.viewport.y);
+    this.ctx.lineTo(universeWidth + this.viewport.x, universeHeight + this.viewport.y);
+
+    this.ctx.closePath();
+
+    this.ctx.strokeStyle = strokeColor;
+    this.ctx.lineWidth = 1;
+    this.ctx.stroke();
+
   }
 
   drawShip(ship: Ship) {
+
     this.ctx.save();
     this.ctx.translate(ship.x + this.viewport.x, ship.y + this.viewport.y);
     this.ctx.rotate(ship.angle);
     this.ctx.lineWidth = 3;
+    this.ctx.strokeStyle = '#000000';
     this.ctx.beginPath();
     this.ctx.moveTo(0, 0);
     this.ctx.lineTo(ship.size / 1.2, 0);
